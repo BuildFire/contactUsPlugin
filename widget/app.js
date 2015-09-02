0412700e0f41e41c49bb027f9aba98fe2d44102a
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-09-02 15:15:02 +0530

    Restructured widget section. Added angular app, template, services etc. Removed previous controller and css file

--- widget/app.js
+++ widget/app.js
@@ -1,28 +1,36 @@
-var WidgetsApp = angular.module('widgetsapp',['ngRoute','widgetappmod','widgetsermod','mapModule']);
+'use strict';
 
-
-WidgetsApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-
-   /*     .when('/try', {
-        templateUrl: 'try.html',
-        controller : ''
-    })   */
-
-        .when('/layout1', {
-            templateUrl: 'templates/layout1.html',
-            controller : 'layout1Ctrl',
-            cache :false,
-            directive :'mapdirective'
+(function (angular, buildfire) {
+  angular.module('contactUsPluginWidget', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'templates/home.html',
+          controllerAs: 'WidgetHome',
+          controller: 'WidgetHomeCtrl'
         })
-
-        .when('/layout2', {
-            templateUrl: 'templates/layout2.html',
-            controller : 'layout2ctrl',
-            cache :false,
-            directive :'mapdirective'
-        })
-
-        .otherwise({redirectTo: '/layout1'});
-}]);
-
+        .otherwise('/');
+    }])
+    .filter('getImageUrl', ['Buildfire', function (Buildfire) {
+      return function (url, width, height, type) {
+        if (type == 'resize')
+          return Buildfire.imageLib.resizeImage(url, {
+            width: width,
+            height: height
+          });
+        else
+          return Buildfire.imageLib.cropImage(url, {
+            width: width,
+            height: height
+          });
+      }
+    }])
+    .directive("buildFireCarousel", ["$rootScope", function ($rootScope) {
+      return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+          $rootScope.$broadcast("Carousel:LOADED");
+        }
+      };
+    }]);
+})(window.angular, window.buildfire);
--- widget/enums.js
+++ widget/enums.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function (angular) {
-  angular.module('contactUsPluginDesign')
+  angular.module('contactUsPluginWidget')
     .constant('TAG_NAMES', {
       CONTACT_INFO: 'ContactInfo'
     })
