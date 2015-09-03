4a7609342209b659458129d839e0e8ee32387a1f
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-09-03 09:30:22 +0530

    added init method and datastore schema for contact tag in design section

--- control/design/controllers/design.home.controller.js
+++ control/design/controllers/design.home.controller.js
@@ -3,8 +3,98 @@
 (function (angular, window) {
   angular
     .module('contactUsPluginDesign')
-    .controller('DesignHomeCtrl', ['$scope', 'DataStore', 'ImageLibrary', 'TAG_NAMES','LAYOUTS',
-      function ($scope, DataStore, ImageLibrary, TAG_NAMES, LAYOUTS) {
+    .controller('DesignHomeCtrl', ['$scope','Buildfire','LAYOUTS','DataStore','TAG_NAMES','STATUS_CODE',
+      function ($scope, Buildfire, LAYOUTS,DataStore,TAG_NAMES,STATUS_CODE) {
+        var _data = {
+          "content": {
+            "carouselImages": [],
+            "description": '<p>&nbsp;<br></p>',
+            "addressTitle": "",
+            "address": {},
+            "links" : []
+          },
+          "design": {
+            "listLayout": LAYOUTS.listLayouts[0].name,
+            "itemBgImage": ""
+          }
+        };
+        var ContentHome = this;
+        ContentHome.masterData = null;
+        ContentHome.data = angular.copy(_data);
+
+        updateMasterItem(_data);
+
+        function updateMasterItem(data) {
+          ContentHome.masterData = angular.copy(data);
+        }
+
+        function isUnchanged(data) {
+          return angular.equals(data, ContentHome.masterData);
+        }
+
+        /*
+         * Go pull any previously saved data
+         * */
+        var init = function () {
+          var success = function (result) {
+              console.info('init success result:', result);
+              ContentHome.data = result.data;
+              updateMasterItem(ContentHome.data);
+              if (tmrDelay)clearTimeout(tmrDelay);
+            }
+            , error = function (err) {
+              if (err && err.code !== STATUS_CODE.NOT_FOUND) {
+                console.error('Error while getting data', err);
+                if (tmrDelay)clearTimeout(tmrDelay);
+              }
+              else if (err && err.code === STATUS_CODE.NOT_FOUND) {
+                saveData(JSON.parse(angular.toJson(ContentHome.data)), TAG_NAMES.CONTACT_INFO);
+              }
+            };
+          DataStore.get(TAG_NAMES.CONTACT_INFO).then(success, error);
+        };
+        init();
+
+        /*
+         * Call the datastore to save the data object
+         */
+        var saveData = function (newObj, tag) {
+          if (typeof newObj === 'undefined') {
+            return;
+          }
+          var success = function (result) {
+              console.info('Saved data result: ', result);
+              updateMasterItem(newObj);
+            }
+            , error = function (err) {
+              console.error('Error while saving data : ', err);
+            };
+          DataStore.save(newObj, tag).then(success, error);
+        };
+
+        /*
+         * create an artificial delay so api isnt called on every character entered
+         * */
+        var tmrDelay = null;
+        var saveDataWithDelay = function (newObj) {
+          if (newObj) {
+            if (isUnchanged(newObj)) {
+              return;
+            }
+            if (tmrDelay) {
+              clearTimeout(tmrDelay);
+            }
+            tmrDelay = setTimeout(function () {
+              saveData(JSON.parse(angular.toJson(newObj)), TAG_NAMES.CONTACT_INFO);
+            }, 500);
+          }
+        };
+        /*
+         * watch for changes in data and trigger the saveDataWithDelay function on change
+         * */
+        $scope.$watch(function () {
+          return ContentHome.data;
+        }, saveDataWithDelay, true);
 
       }]);
 })(window.angular, window);
