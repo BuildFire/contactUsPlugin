cc919ec2f86d2e462129c38eb90df169f7709c64
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-09-07 17:24:50 +0530

    Implemented buildfire component carousel in widget layout 1

--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -2,37 +2,67 @@
 
 (function (angular) {
   angular.module('contactUsPluginWidget')
-    .controller('WidgetHomeCtrl', ['$routeParams', 'Buildfire', 'DataStore','$scope', 'TAG_NAMES', 'Location', 'LAYOUTS', function ($routeParams,Buildfire, DataStore, $scope, TAG_NAMES, Location, LAYOUTS) {
+    .controller('WidgetHomeCtrl', ['$routeParams', 'Buildfire', 'DataStore', '$scope', 'TAG_NAMES', 'Location', 'LAYOUTS', '$rootScope',
+      function ($routeParams, Buildfire, DataStore, $scope, TAG_NAMES, Location, LAYOUTS, $rootScope) {
         var WidgetHome = this;
-        var DesignHomeMaster;
-        function widgetInit() {
-          WidgetHome.data = {};
-          Buildfire.datastore.get(TAG_NAMES.CONTACT_INFO, function (err, data) {
-            if (err) {
-              Console.log('------------Error in Design of People Plugin------------', err);
-            }
+        var currentListLayout = null;
+        WidgetHome.data = null;
+        //create new instance of buildfire carousel viewer
+        var view = null;
 
-            else if (data && data.data) {
-              console.log("Widget:",data)
-              WidgetHome.contactUsDesignData = angular.copy(data.data);
-              if (!WidgetHome.contactUsDesignData.design)
-                WidgetHome.contactUsDesignData.design = {};
-              if (!WidgetHome.contactUsDesignData.design.listLayout)
-                WidgetHome.contactUsDesignData.design.listLayout = WidgetHome.layouts.listLayouts[0].name;
-              DesignHomeMaster = angular.copy(data.data);
+        /*
+         * Fetch user's data from datastore
+         */
 
+        var init = function () {
+          var success = function (result) {
+              WidgetHome.data = result.data;
+              if (!WidgetHome.data.design)
+                WidgetHome.data.design = {};
+              if (!WidgetHome.data.design.listLayout) {
+                WidgetHome.data.design.listLayout = LAYOUTS.listLayouts[0].name;
+              }
+              currentListLayout = WidgetHome.data.design.listLayout;
             }
-            else {
-              WidgetHome.contactUsDesignData = contactUsDesignData;
-              console.info('------------------unable to load data---------------');
-            }
-          });
-        }
-        widgetInit();
-        Buildfire.datastore.onUpdate(function (event) {
-          widgetInit();
-          $scope.$apply();
+            , error = function (err) {
+              console.error('Error while getting data', err);
+            };
+          DataStore.get(TAG_NAMES.CONTACT_INFO).then(success, error);
+        };
+        init();
+        $rootScope.$on("Carousel:LOADED", function () {
+          if (!view) {
+            view = new buildfire.components.carousel.view("#carousel", []);
+          }
+          if (WidgetHome.data.content && WidgetHome.data.content.carouselImages) {
+            view.loadItems(WidgetHome.data.content.carouselImages);
+          } else {
+            view.loadItems([]);
+          }
         });
 
-    }])
+        var onUpdateCallback = function (event) {
+          if (event && event.tag === TAG_NAMES.CONTACT_INFO) {
+            WidgetHome.data = event.data;
+            if (!WidgetHome.data.design)
+              WidgetHome.data.design = {};
+          }
+          if (currentListLayout != WidgetHome.data.design.listLayout && view && WidgetHome.data.content.carouselImages) {
+            view._destroySlider();
+            view = null;
+          }
+          else {
+            if (view) {
+              view.loadItems(WidgetHome.data.content.carouselImages);
+            }
+          }
+          currentListLayout = WidgetHome.data.design.listLayout;
+        };
+        DataStore.onUpdate().then(null, null, onUpdateCallback);
+
+        $scope.$on("$destroy", function () {
+          DataStore.clearListener();
+        });
+
+      }])
 })(window.angular);
