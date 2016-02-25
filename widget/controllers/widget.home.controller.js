503c188581ce65c1c6c6005fe4b42e01eff630d3
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-02-25 14:07:17 +0530

    Fixed test cases

--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -152,9 +152,9 @@
 
         WidgetHome.onAddressClick = function (long, lat) {
             if (WidgetHome.device && WidgetHome.device.platform == 'ios')
-              Buildfire.navigation.openWindow("maps://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
+              buildfire.navigation.openWindow("maps://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
           else
-              Buildfire.navigation.openWindow("http://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
+              buildfire.navigation.openWindow("http://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
          }
 
       }])
