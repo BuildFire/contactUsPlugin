78ea349ba6f2333f269d5541f2eb5832c01b0a5c
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-03-14 16:36:28 +0530

    Made required changes for pivotal issue: #115494479

--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -134,8 +134,16 @@
           DataStore.clearListener();
         });
         WidgetHome.safeHtml = function (html) {
-          if (html)
-            return $sce.trustAsHtml(html);
+            if (html) {
+                var $html = $('<div />', {html: html});
+                $html.find('iframe').each(function (index, element) {
+                    var src = element.src;
+                    console.log('element is: ', src, src.indexOf('http'));
+                    src = src && src.indexOf('file://') != -1 ? src.replace('file://', 'http://') : src;
+                    element.src = src && src.indexOf('http') != -1 ? src : 'http:' + src;
+                });
+                return $sce.trustAsHtml($html.html());
+            }
         };
 
         WidgetHome.openLinks = function (actionItems) {
