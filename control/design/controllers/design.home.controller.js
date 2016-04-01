566e9a08f806b4f5e7f3874323e0adc651835fef
Vishnu Singh <vish250491@gmail.com>
2016-04-01 16:58:46 +0530

    init data functionality provided

--- control/design/controllers/design.home.controller.js
+++ control/design/controllers/design.home.controller.js
@@ -13,6 +13,44 @@
             {name: "Layout_2"}
           ]
         };
+
+        //init dummy data
+        var _dummyData= {
+          content: {
+            showMap:true,
+            carouselImages: [{
+              action: "noAction",
+              iconUrl: "http://fortunednagroup.com/wp-content/uploads/employee1.jpg",
+              title: "image"
+            },
+              {
+                action: "noAction",
+                iconUrl: "http://smslaw.com.au/wp-content/uploads/2014/07/employee.jpg",
+                title: "image"
+              },
+              {
+                action: "noAction",
+                iconUrl: "http://cdn2.hubspot.net/hub/105358/file-486221503-png/images/winter-blues-how-to-keep-your-employees-motivated-and-productive.png?t=1458850411530",
+                title: "image"
+              }],
+            description : "<p>World IT technology</p>",
+            addressTitle:"London",
+            address:{
+              type:"Location",
+              location:"London, UK",
+              location_coordinates:[-0.12775829999998223, 51.5073509]
+            },
+            links:[]
+          },
+          design:{
+            listLayout:"Layout_1",
+            backgroundImage:"http://buildfire.imgix.net/b55ee984-a8e8-11e5-88d3-124798dea82d/d3a73620-f7f4-11e5-a9d8-55461c8fe352.jpg?w=88"
+          }
+        };
+
+        DesignHomeMaster=_dummyData;
+
+
         /*On layout click event*/
         DesignHome.changeItemLayout = function (layoutName) {
           if (layoutName && DesignHome.data.design) {
@@ -56,16 +94,7 @@
         };
 
         function init() {
-          var _data = {
-            design: {
-              listLayout: "",
-              backgroundImage: ""
-            },
-            content: {
-              images: [],
-              description: ""
-            }
-          };
+          var _data = _dummyData;
 
           /* background image add </end>*/
           Buildfire.datastore.get(TAG_NAMES.CONTACT_INFO, function (err, data) {
