ffc2c3750709fe47463567bf170e23bc3d82a03d
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-06-08 09:44:09 +0530

    Handled some cases for pull to refresh.

--- widget/controllers/widget.home.controller.js
+++ widget/controllers/widget.home.controller.js
@@ -8,48 +8,62 @@
         var currentListLayout = null;
         WidgetHome.data = {};
 
+        //create new instance of buildfire carousel viewer
+        WidgetHome.view = null;
+
         //Refresh data on pulling the tile bar
 
         buildfire.datastore.onRefresh(function () {
-          init();
-          $scope.$digest();
+          init(function (err) {
+            if (!err) {
+              if (!WidgetHome.view) {
+                WidgetHome.view = new Buildfire.components.carousel.view("#carousel", []);
+              }
+              if (WidgetHome.data.content && WidgetHome.data.content.carouselImages) {
+                WidgetHome.view.loadItems(WidgetHome.data.content.carouselImages);
+              } else {
+                WidgetHome.view.loadItems([]);
+              }
+            }
+          });
         });
 
-        var _dummyData= {
+        var _dummyData = {
           content: {
-             showMap:true,
-              carouselImages: [{
-                  action: "noAction",
-                  iconUrl: "http://buildfire.imgix.net/1462345835888-04866688400506973/6ac49110-11c7-11e6-92ea-27ed66023d52.jpeg?fit=crop&w=342&h=193",
-                  title: "image"
-              },
-                  {
-                      action: "noAction",
-                      iconUrl: "http://buildfire.imgix.net/1462345835888-04866688400506973/6bf3c240-11c7-11e6-ad08-375cc71b6ca7.jpg?fit=crop&w=342&h=193",
-                      title: "image"
-                  }],
-              description : "<p>With the wysiwyg, you can include text and lists, embed images, embed videos, and link to webpages, emails, phone numbers and more. Check out the tutorial on the wysiwyg for detailed information.</p>",
-              addressTitle:"",
-              address:{
-                  type:"Location",
-                  location:"501 Pacific Hwy, San Diego, CA 92101, USA",
-                  location_coordinates:[-117.17096400000003,32.7100444]
-              },
-            links:[{"title":"Call","action":"callNumber","phoneNumber":"6195551234"},{"title":"Email","action":"sendEmail"}]
+            showMap: true,
+            carouselImages: [{
+              action: "noAction",
+              iconUrl: "http://buildfire.imgix.net/1462345835888-04866688400506973/6ac49110-11c7-11e6-92ea-27ed66023d52.jpeg?fit=crop&w=342&h=193",
+              title: "image"
+            },
+              {
+                action: "noAction",
+                iconUrl: "http://buildfire.imgix.net/1462345835888-04866688400506973/6bf3c240-11c7-11e6-ad08-375cc71b6ca7.jpg?fit=crop&w=342&h=193",
+                title: "image"
+              }],
+            description: "<p>With the wysiwyg, you can include text and lists, embed images, embed videos, and link to webpages, emails, phone numbers and more. Check out the tutorial on the wysiwyg for detailed information.</p>",
+            addressTitle: "",
+            address: {
+              type: "Location",
+              location: "501 Pacific Hwy, San Diego, CA 92101, USA",
+              location_coordinates: [-117.17096400000003, 32.7100444]
+            },
+            links: [{
+              "title": "Call",
+              "action": "callNumber",
+              "phoneNumber": "6195551234"
+            }, {"title": "Email", "action": "sendEmail"}]
           },
-          design:{
-            listLayout:"Layout_1",
-              backgroundImage:""
+          design: {
+            listLayout: "Layout_1",
+            backgroundImage: ""
           }
         };
-
-        //create new instance of buildfire carousel viewer
-        WidgetHome.view = null;
 
         /*declare the device width heights*/
         $rootScope.deviceHeight = window.innerHeight;
         $rootScope.deviceWidth = window.innerWidth || 320;
-        $rootScope.backgroundImage="";
+        $rootScope.backgroundImage = "";
         WidgetHome.device = null;
         /*initialize the device width heights*/
         function initDeviceSize(callback) {
@@ -89,57 +103,58 @@
         /*
          * Fetch user's data from datastore
          */
-        var init = function () {
+        var init = function (cb) {
           var success = function (result) {
-                if(!result.id){
-                  console.log('NO DATA AVAILABLE');
-                  WidgetHome.data=_dummyData;
-                }else{
-                  WidgetHome.data = result.data;
-                }
-                if (!WidgetHome.data.design){
-                  WidgetHome.data.design = {};
-                }
-                if (!WidgetHome.data.design.listLayout) {
-                  WidgetHome.data.design.listLayout = LAYOUTS.listLayouts[0].name;
-                }
-                currentListLayout = WidgetHome.data.design.listLayout;
-                if(WidgetHome.data.design.backgroundImage){
-                  $rootScope.backgroundImage = WidgetHome.data.design.backgroundImage;
-                }
+              if (!result.id) {
+                console.log('NO DATA AVAILABLE');
+                WidgetHome.data = _dummyData;
+              } else {
+                WidgetHome.data = result.data;
+              }
+              if (!WidgetHome.data.design) {
+                WidgetHome.data.design = {};
+              }
+              if (!WidgetHome.data.design.listLayout) {
+                WidgetHome.data.design.listLayout = LAYOUTS.listLayouts[0].name;
+              }
+              currentListLayout = WidgetHome.data.design.listLayout;
+              if (WidgetHome.data.design.backgroundImage) {
+                $rootScope.backgroundImage = WidgetHome.data.design.backgroundImage;
+              }
+              else {
+                $rootScope.backgroundImage = "";
+              }
+              var getDevice = function (error, data) {
+                if (data)
+                  WidgetHome.device = data.device;
                 else
-                {
-                  $rootScope.backgroundImage = "";
-                }
-                var getDevice = function(error, data){
-                  if(data)
-                    WidgetHome.device  = data.device;
-                  else
-                    console.log("Error while getting the device context data", error)
-                };
-                buildfire.getContext(getDevice);
+                  console.log("Error while getting the device context data", error)
+              };
+              buildfire.getContext(getDevice);
+              cb();
 
             }
             , error = function (err) {
               console.error('Error while getting data', err);
+              cb(err);
             };
           DataStore.get(TAG_NAMES.CONTACT_INFO).then(success, error);
         };
         init();
-        $scope.$on('$viewContentLoaded', function(){
-        $rootScope.$on("Carousel:LOADED", function () {
-          if (!WidgetHome.view) {
-            WidgetHome.view = new Buildfire.components.carousel.view("#carousel", []);
-          }
-          if (WidgetHome.data.content && WidgetHome.data.content.carouselImages) {
-            WidgetHome.view.loadItems(WidgetHome.data.content.carouselImages);
-          } else {
-            WidgetHome.view.loadItems([]);
-          }
-        });
+        $scope.$on('$viewContentLoaded', function () {
+          $rootScope.$on("Carousel:LOADED", function () {
+            if (!WidgetHome.view) {
+              WidgetHome.view = new Buildfire.components.carousel.view("#carousel", []);
+            }
+            if (WidgetHome.data.content && WidgetHome.data.content.carouselImages) {
+              WidgetHome.view.loadItems(WidgetHome.data.content.carouselImages);
+            } else {
+              WidgetHome.view.loadItems([]);
+            }
+          });
         });
 
-       /* var onUpdateCallback = function (event) {
+        var onUpdateCallback = function (event) {
           setTimeout(function () {
             $scope.imagesUpdated = false;
             $scope.$digest();
@@ -150,10 +165,9 @@
               if (!WidgetHome.data.content)
                 WidgetHome.data.content = {};
             }
-            if(WidgetHome.data.design.backgroundImage){
+            if (WidgetHome.data.design.backgroundImage) {
               $rootScope.backgroundImage = WidgetHome.data.design.backgroundImage;
-            }else
-            {
+            } else {
               $rootScope.backgroundImage = "";
             }
             if (!WidgetHome.data.design.listLayout) {
@@ -174,22 +188,22 @@
             $rootScope.$digest();
           }, 0);
         };
-        DataStore.onUpdate().then(null, null, onUpdateCallback);*/
+        DataStore.onUpdate().then(null, null, onUpdateCallback);
 
         $scope.$on("$destroy", function () {
           DataStore.clearListener();
         });
         WidgetHome.safeHtml = function (html) {
-            if (html) {
-                var $html = $('<div />', {html: html});
-                $html.find('iframe').each(function (index, element) {
-                    var src = element.src;
-                    console.log('element is: ', src, src.indexOf('http'));
-                    src = src && src.indexOf('file://') != -1 ? src.replace('file://', 'http://') : src;
-                    element.src = src && src.indexOf('http') != -1 ? src : 'http:' + src;
-                });
-                return $sce.trustAsHtml($html.html());
-            }
+          if (html) {
+            var $html = $('<div />', {html: html});
+            $html.find('iframe').each(function (index, element) {
+              var src = element.src;
+              console.log('element is: ', src, src.indexOf('http'));
+              src = src && src.indexOf('file://') != -1 ? src.replace('file://', 'http://') : src;
+              element.src = src && src.indexOf('http') != -1 ? src : 'http:' + src;
+            });
+            return $sce.trustAsHtml($html.html());
+          }
         };
 
         WidgetHome.openLinks = function (actionItems) {
@@ -205,11 +219,11 @@
         };
 
         WidgetHome.onAddressClick = function (long, lat) {
-            if (WidgetHome.device && WidgetHome.device.platform == 'ios')
-              buildfire.navigation.openWindow("maps://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
+          if (WidgetHome.device && WidgetHome.device.platform == 'ios')
+            buildfire.navigation.openWindow("maps://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
           else
-              buildfire.navigation.openWindow("http://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
-         }
+            buildfire.navigation.openWindow("http://maps.google.com/maps?daddr=" + lat + "," + long, '_system');
+        }
 
       }])
 })(window.angular, window.buildfire);
